1c4941a7bf0aa9351ec3ccc4611491752cff8867
Oliver.Mengich <kemeioliver2000@gmail.com>
2021-01-11 13:52:09 +0300

    Removing Character on String

---
+++ RemoveCharacter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("Enter The Required Text");
+            string text = Console.ReadLine();
+            Console.WriteLine("Which Text do you want to remove? Enter Text number \n Smaller than"+ text.Length);
+            
+            int num = int.Parse(Console.ReadLine());
+            if (num> text.Length)
+            {
+                Console.WriteLine("Enter a small number");
+                goto start;
+            }
+            var result = text.Remove(num,1);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
